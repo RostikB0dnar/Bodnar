41886d19aa1214429aee8416235b885a6a8cbc73
RostikB0dnar <155552585+RostikB0dnar@users.noreply.github.com>
2024-10-12 17:57:59 +0300

    Add files via upload

---
+++ Lab2.cs
@@ -0,0 +1,18 @@
+﻿public class Lab2
+{
+    public static void Main()
+    {
+        int[] numbers = new int[10];
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = i + 1;
+        }
+
+        Console.WriteLine("Array elements:");
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            Console.WriteLine($"Element {i + 1}: {numbers[i]}");
+        }
+    }
+}
