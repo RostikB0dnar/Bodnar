2726c0235070589da48ee3e38638e34203fbc9e9
RostikB0dnar <155552585+RostikB0dnar@users.noreply.github.com>
2024-10-28 17:30:53 +0200

    Add files via upload

---
+++ Lab7.cs
@@ -0,0 +1,26 @@
+﻿
+class Program
+{
+    static void PrintNumbersInRange(int start, int end)
+    {
+        for (int i = start; i <= end; i++)
+        {
+            Console.WriteLine($"Потік {Thread.CurrentThread.ManagedThreadId}: {i}");
+            Thread.Sleep(100); 
+        }
+    }
+
+    static void Main()
+    {
+        Thread thread1 = new Thread(() => PrintNumbersInRange(0, 10));
+        Thread thread2 = new Thread(() => PrintNumbersInRange(11, 20));
+
+        thread1.Start();
+        thread2.Start();
+
+        thread1.Join();
+        thread2.Join();
+
+        Console.WriteLine("Основний потік завершений.");
+    }
+}
