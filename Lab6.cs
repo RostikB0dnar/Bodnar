622ebaa2a5835992956d7f6ba5b85eff6cd7c475
RostikB0dnar <155552585+RostikB0dnar@users.noreply.github.com>
2024-10-28 17:27:23 +0200

    Add files via upload

---
+++ Lab6.cs
@@ -0,0 +1,18 @@
+﻿class Program
+{
+    public delegate int SumDelegate(int a, int b);
+
+    static void Main()
+    {
+        SumDelegate sumDelegate = Add;
+
+        Console.WriteLine("Сума 3 і 5: " + sumDelegate(3, 5));
+        Console.WriteLine("Сума 10 і 20: " + sumDelegate(10, 20));
+        Console.WriteLine("Сума -7 і 12: " + sumDelegate(-7, 12));
+    }
+
+    public static int Add(int a, int b)
+    {
+        return a + b;
+    }
+}
