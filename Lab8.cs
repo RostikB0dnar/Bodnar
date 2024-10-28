c4e609d7c47bcf15b4f8ced79fe51dae48172f11
RostikB0dnar <155552585+RostikB0dnar@users.noreply.github.com>
2024-10-28 17:34:22 +0200

    Add files via upload

---
+++ Lab8.cs
@@ -0,0 +1,23 @@
+﻿class Program
+{
+    public static async Task<string> LoadDataFromServerAsync()
+    {
+        Console.WriteLine("Завантаження даних із сервера...");
+
+        await Task.Delay(3000);
+
+        Console.WriteLine("Завантаження завершено.");
+        return "Дані з сервера";
+    }
+
+    static async Task Main(string[] args)
+    {
+        Console.WriteLine("Початок програми");
+
+        string data = await LoadDataFromServerAsync();
+
+        Console.WriteLine($"Отримані дані: {data}");
+
+        Console.WriteLine("Кінець програми");
+    }
+}
